9166e0499c667954bf00eadac1d9057d57ca4bc6
Sean Marlow <sean.marlow@suse.com>
2019-07-15 15:05:55 -0500

    Remove python 3.4 support.

--- setup.py
+++ setup.py
@@ -76,7 +76,6 @@ if __name__ == '__main__':
             'GNU General Public License v3 or later (GPLv3+)',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
